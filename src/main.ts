07778af038d4891f4acfe789dfead9e2ba6e07fe
bd_ <bdunderscore@fushizen.net>
2022-09-09 08:28:43 -0700

    Set up dist/-generating workflow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const output: string = core.getInput('output');
+    const repository: string = core.getInput('repository');
+    const include_prereleases_s: string = core.getInput('include_prereleases');
+    const package_name: string = core.getInput('package');
+    const token: string = core.getInput('token');
+
+    const [ owner, repo ] = repository.split('/');
+
+    const gh = github.getOctokit(token);
+    const releases = await gh.rest.repos.listReleases({ owner, repo });
+    if (releases.status !== 200) throw new Error(`Failed to get releases for ${repository}`);
+    for (const release of releases.data) {
+      let meta_asset = null;
+      release.assets.forEach(asset => {
+        if (asset.name === 'vpm-metadata.json') {
+          meta_asset = asset;
+        }
+      });
+
+      // fetch and parse metadata file
+      // update/generate listing
+    }
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
