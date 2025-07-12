399a2253a51ecdb30e4efa7b5161dd827fe9f2bc
bd_ <bd_@nadena.dev>
2025-07-12 14:24:26 -0700

    Cache results; generate zipSHA256

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as fs from 'fs';
 import fetch from 'node-fetch';
+import { VPMPackage, VPMRepository } from './repo';
 
 interface Package {
     readonly name: string;
@@ -22,29 +23,30 @@ async function getJson(url: string): Promise<unknown> {
     return await res.json();
 }
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
     try {
         const output: string = core.getInput('output');
         const repository: string = core.getInput('repository');
         const package_name: string = core.getInput('package');
         const token: string = core.getInput('token');
         const repo_url: string = core.getInput('repo_url');
-        const prerelease_repo_url: string = core.getInput(
-            'prerelease_repo_url'
-        );
         const repo_author: string = core.getInput('repo_author');
         const repo_name: string = core.getInput('repo_name');
-        let prerelease_repo_name: string = core.getInput(
-            'prerelease_repo_name'
-        );
-
         const repo_id: string = core.getInput('repo_id');
-        let prerelease_repo_id: string = core.getInput('prerelease_repo_id');
 
         const [owner, repo] = repository.split('/');
 
         const newPackages: Package[] = [];
         const prereleasePackages: Package[] = [];
+
+        const repo_file_name = new URL(repo_url).pathname.split('/').pop() as string;
+        const repo_file_path = `${output}/${repo_file_name}`;
+        const vpm_repo = new VPMRepository(repo_file_path, {
+            author: repo_author,
+            name: repo_name,
+            id: repo_id,
+            url: repo_url
+        });
 
         const gh = github.getOctokit(token);
         const releases = await gh.paginate(gh.rest.repos.listReleases, {
@@ -52,6 +54,7 @@ async function run(): Promise<void> {
             repo
         });
         console.log(releases);
+
         for (const release of releases) {
             if (release.draft) continue;
 
@@ -80,91 +83,49 @@ async function run(): Promise<void> {
                 continue;
             }
 
-            const packageInfo_ = await getJson(meta_asset.browser_download_url);
-            if (typeof packageInfo_ !== 'object') {
-                console.error(
-                    `[ERROR] Failed to parse package.json for release ${release.name}`
-                );
-                console.log(`Got: ${JSON.stringify(packageInfo_)}`);
-                continue;
+            let zipSHA256: string | undefined = undefined;
+
+            const digest = (package_zip as any).digest;
+            if (digest !== null) {
+                if (digest.startsWith('sha256:')) {
+                    zipSHA256 = digest.slice(7);
+                }
             }
 
-            const packageInfo = packageInfo_ as Package;
+            const get_package_info = async function() {
+                console.log(`[INFO] Fetching package info for ${release.name}`);
+                const packageInfo_ = await getJson(meta_asset!.browser_download_url);
+                if (typeof packageInfo_ !== 'object') {
+                    console.error(
+                        `[ERROR] Failed to parse package.json for release ${release.name}`
+                    );
+                    console.log(`Got: ${JSON.stringify(packageInfo_)}`);
+                    return null;
+                }
 
-            packageInfo['url'] = `${package_zip.browser_download_url}?`;
-            packageInfo['repo'] = repo_url;
-            prereleasePackages.push(packageInfo);
-            if (!release.prerelease) {
-                newPackages.push(packageInfo);
+                const packageInfo = packageInfo_ as VPMPackage;
+
+                packageInfo['url'] = `${package_zip!.browser_download_url}?`;
+                packageInfo['repo'] = repo_url;
+                packageInfo['zipSHA256'] = zipSHA256;
+
+                console.log(`[INFO] Package info for ${release.name}: ${JSON.stringify(packageInfo)}`);
+
+                return packageInfo;
             }
+
+            await vpm_repo.addPackage({
+                name: package_name,
+                version: release.tag_name,
+                url: package_zip.browser_download_url,
+                package_info: get_package_info
+            });
         }
 
-        if (prerelease_repo_name === '') {
-            prerelease_repo_name = `${repo_name} (prereleases)`;
-        }
-
-        if (!prerelease_repo_id && repo_id) {
-            prerelease_repo_id = `${repo_id}.prereleases`;
-        }
-
-        update_repo(
-            output,
-            repo_url,
-            repo_author,
-            repo_name,
-            package_name,
-            repo_id,
-            newPackages
-        );
-        update_repo(
-            output,
-            prerelease_repo_url,
-            `${repo_author} (prerelease)`,
-            prerelease_repo_name,
-            package_name,
-            prerelease_repo_id,
-            prereleasePackages
-        );
+        vpm_repo.save();
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message);
     }
-}
-
-function update_repo(
-    output: string,
-    url: string,
-    author: string,
-    name: string,
-    package_name: string,
-    repo_id: string,
-    packages: Package[]
-): void {
-    if (url === '') return;
-
-    const filename = new URL(url).pathname.split('/').pop() as string;
-    const output_path = `${output}/${filename}`;
-
-    const repoInfo = {
-        packages: {} as {
-            [key: string]: {versions: {[version: string]: unknown}};
-        },
-        author,
-        name,
-        id: repo_id ? repo_id : undefined, // to remove id property if not specified
-        url
-    };
-    if (fs.existsSync(output_path)) {
-        const priorRepoInfo = JSON.parse(fs.readFileSync(output_path, 'utf8'));
-        repoInfo.packages = priorRepoInfo.packages;
-    }
-
-    for (const pkg of packages) {
-        if (repoInfo.packages[pkg.name] === undefined)
-            repoInfo.packages[pkg.name] = {versions: {}};
-        repoInfo.packages[pkg.name].versions[pkg.version] = pkg;
-    }
-
-    fs.writeFileSync(output_path, JSON.stringify(repoInfo, null, 2));
 }
 
 run();
---
+++ src/repo.ts
@@ -0,0 +1,113 @@
+import { createHash } from "crypto";
+import { version } from "os";
+import * as fs from "fs";
+
+export interface Package {
+    readonly name: string;
+    readonly version: string;
+    url: string;
+    package_info: () => Promise<VPMPackage | null>;
+}
+
+export interface VPMPackage {
+    name?: string;
+    version?: string;
+    displayName?: string;
+    description?: string;
+    url?: string;
+    repo?: string;
+    zipSHA256?: string;
+
+    [key: string]: unknown;
+}
+
+export interface VPMRepositoryData {
+    packages?: { [key: string]: { versions: {[version: string]: VPMPackage} } };
+    author?: string;
+    name?: string;
+    id?: string;
+    url?: string;
+}
+
+export interface VPMRepositoryInit {
+    author: string;
+    name: string;
+    id?: string;
+    url: string;
+}
+
+export class VPMRepository {
+    readonly file_path: string;
+    readonly data: VPMRepositoryData;
+
+    constructor(path: string, init: VPMRepositoryInit) {
+        this.file_path = path;
+
+        if (fs.existsSync(path)) {
+            this.data = JSON.parse(fs.readFileSync(path, 'utf-8'));
+        } else {
+            this.data = { ...init, packages: {} };
+        }
+    }
+
+    async fetchAndComputeSha256(url: string): Promise<string | undefined> {
+        try {
+            const res = await fetch(url);
+            if (!res.ok) {
+                throw new Error(`Failed to fetch ${url}: ${res.status} ${res.statusText}`);
+            }
+            const arrayBuffer = await res.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+            const hash = createHash('sha256').update(buffer).digest('hex');
+            return hash;
+        } catch (error) {
+            console.error(`Error fetching or computing SHA256 for ${url}:`, error);
+            return undefined;
+        }
+    }
+
+    async addPackage(pkg: Package): Promise<void> {
+        let version_info: VPMPackage | null = this.data.packages![pkg.name]?.versions[pkg.version];
+
+        if (version_info === undefined) {
+            console.log(`[INFO] Adding package ${pkg.name}@${pkg.version} to the repository.`);
+            version_info = await pkg.package_info();
+
+            if (version_info === null) {
+                console.log(`[ERROR] Failed to retrieve package info for ${pkg.name}@${pkg.version}`);
+                return;
+            }
+
+            if (this.data.packages![pkg.name] === undefined) {
+                this.data.packages![pkg.name] = { versions: {} };
+            }
+
+            if (version_info.name === undefined) {
+                version_info.name = pkg.name;
+            }
+
+            if (version_info.name !== pkg.name || version_info.version !== pkg.version) {
+                throw new Error(`Package name or version mismatch: expected ${pkg.name}@${pkg.version}, got ${version_info.name}@${version_info.version}`);
+            }
+
+            this.data.packages![pkg.name].versions[pkg.version] = version_info;
+
+            if (version_info.repo === null) {
+                version_info.repo = this.data.url;
+            }
+        }
+
+        if (version_info.zipSHA256 === undefined) {
+            version_info.zipSHA256 = await this.fetchAndComputeSha256(pkg.url);
+            console.log(`[INFO] Computed SHA256 for ${pkg.url}: ${version_info.zipSHA256}`);
+            if (version_info.zipSHA256 === null) {
+                console.error(`[ERROR] Failed to compute SHA256 for ${pkg.url}`);
+                return;
+            }
+        }
+    }
+
+    save(): void {
+        fs.writeFileSync(this.file_path, JSON.stringify(this.data, null, 2));
+    }
+}
